ff35f6dc927fe984bfc44f4f6d9e01cfef79f11c
anjaliToqnoq <anjalig@toqnoq.com>
2026-09-14 15:20:42 +0530

    Add Main class with greeting and role display

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Team!");
+        System.out.println("This is Anjali's first Git collaboration project.");
+
+        String name = "Anjali";
+        String role = "Backend developer";
+
+        System.out.println("Name: " + name);
+        System.out.println("Role: " + role);
+    }
+}
